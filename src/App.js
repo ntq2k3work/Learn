67c9f72ecdbc81ac7c7942967a77994cbdd7a73a
ntq2k3work <ntq2k3.work@gmail.com>
2023-11-15 20:29:43 +0700

    react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.scss';
 import Variable from "./view/Example/variables";
 import UseState from './view/Example/state';
+function MyButton() {
+  return (
+    <h1>HEHEHE</h1>
+  );
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Variable />
         <UseState />
+        <MyButton />
       </header>
     </div>
   );
--- src/view/Example/state.js
+++ src/view/Example/state.js
@@ -1,4 +1,4 @@
-// State
+// State chủ yếu được sử dụng để cập nhật component khi người dùng thực hiện một số hành động như nhấp vào nút, nhập một số văn bản, nhấn một số phím
 
 import React, { Component } from 'react';
 class UseState extends Component {
@@ -17,7 +17,7 @@ class UseState extends Component {
             <>
                 <div>
                     <h4>Nhập tên</h4>
-                    <input type='text' className='inputState' onChange={(event) => this.handleOnchangeName(event)} />
+                    <input type='text' className='inputState' onChange={(event) => this.handleOnchangeName(event)} value={this.state.name}  />
                 </div>
                 <div>
                     My name is {this.state.name} <br/>
